998464e82707d111fdcdf46c675886376eb451f6
mmcnabb <>
2017-12-18 10:08:52 -0500

    Update build tasks - add predeploy step

--- psake.ps1
+++ psake.ps1
@@ -2,7 +2,7 @@ properties {
     if ($env:APPVEYOR)
     {
         $ModuleName = $env:APPVEYOR_PROJECT_NAME
-        $Version = $env:APPVEYOR_BUILD_VERSION   
+        $Version = $env:APPVEYOR_BUILD_VERSION
         $TestExit = $true 
     }
     else
@@ -33,12 +33,12 @@ task Compile -Action {
     $ManifestData = Get-Content $BuildManifestPath
     $ManifestData = $ManifestData -replace "ModuleVersion\s+=\s+`"\d+\.\d+\.\d+\.\d+`"", "ModuleVersion = `"$Version`""
     $ManifestData | Out-File $BuildManifestPath -Force -Encoding utf8
-}
+} -description "compiles separate function files into a single module"
 
 task GenerateHelp -Depends Compile -Action {
     $null = New-Item -ItemType Directory -Path $BuildDocsPath -Force
     $null = New-ExternalHelp -Path $ProjectDocsPath -OutPutPath $BuildDocsPath -Encoding ([System.Text.Encoding]::UTF8) -Force    
-}
+} -description "builds external help file from markdown"
 
 task Analyze -depends GenerateHelp -action {
     $AnalyzeSplat = @{
@@ -60,23 +60,30 @@ task Analyze -depends GenerateHelp -action {
             Write-Error @ErrorSplat -ErrorAction Stop
         }
     }
-}
+} -description "runs the PSScriptAnalyzer against the built module"
 
 task TestManifest -depends Analyze -action {
     Test-ModuleManifest -Path $BuildManifestPath -ErrorAction Stop
-}
+} -description "tests the validity of the module manifest"
 
 task Test -Depends GenerateHelp -action {
     Invoke-Pester -Script $PSScriptRoot -EnableExit:$TestExit -PesterOption @{IncludeVSCodeMarker = $true}
-}
+} -description "runs Pester tests"
 
 task Clean -depends Test -action {
     Remove-Item -Path $BuildTempPath -Confirm:$false -Recurse
-}
+} -description "cleans up the build directory"
 
-task Build -depends Clean
+task Build -depends Test
+
+task PreDeploy -action {
+    if (!$env:APPVEYOR_REPO_TAG)
+    {
+        exit 0
+    }
+} -description "checks to see if the build was pushed by a tag"
 
 Task Deploy {
     Import-Module PowerShellGet -Force
     Publish-Module -Path $BuildModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false -Verbose
-}
+} -description "deploys the built module to the Powershell Gallery"
